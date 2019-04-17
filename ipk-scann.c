e2e25381d32c1c873d45b8487f202dc85bc66c4c
Simon Kobyda <skobyda@redhat.com>
2019-04-17 03:35:22 +0200

    Pulled som code into separated functions, also implemented filtered option

--- ipk-scann.c
+++ ipk-scann.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <pcap.h>
-#include <netinet/tcp.h> // Provides declarations for tcp header
-#include <netinet/ip.h>  // Provides declarations for ip header
+#include <netinet/tcp.h>
+#include <netinet/ip.h>
 #include <sys/socket.h>
 #include <errno.h>
 #include <netinet/in.h>
@@ -11,8 +12,6 @@
 #include <arpa/inet.h>
 
 #include <unistd.h>
-// #include <sys/time.h>
-// #include <sys/types.h>
 
 
 /*
@@ -39,6 +38,7 @@
     /* Ethernet addresses are 6 bytes */
 #define ETHER_ADDR_LEN	6
 
+    // SOURCE: https://www.tcpdump.org/pcap.html
 	/* Ethernet header */
 	struct sniff_ethernet {
 		u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
@@ -103,11 +103,8 @@ struct pseudo_header
     u_int16_t tcp_length;
 };
 
-/*
-    Generic checksum calculation function
-*/
-unsigned short csum(unsigned short *ptr,int nbytes)
-{
+// Source: https://www.binarytides.com/raw-sockets-c-code-linux/
+unsigned short csum(unsigned short *ptr,int nbytes) {
     register long sum;
     unsigned short oddbyte;
     register short answer;
@@ -130,211 +127,272 @@ unsigned short csum(unsigned short *ptr,int nbytes)
     return(answer);
 }
 
-void myHandler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
-	const struct sniff_ip *ip; /* The IP header */
-	const struct sniff_tcp *tcp; /* The TCP header */
+void my_handler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
+    const struct sniff_ip *ip; /* The IP header */
+    const struct sniff_tcp *tcp; /* The TCP header */
 
-	u_int size_ip;
-	u_int size_tcp;
+    u_int size_ip;
+    u_int size_tcp;
 
-	ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
-	size_ip = IP_HL(ip)*4;
-	if (size_ip < 20) {
-		printf("   * Invalid IP header length: %u bytes\n", size_ip);
-		return;
-	}
-	tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
-	size_tcp = TH_OFF(tcp)*4;
-	if (size_tcp < 20) {
-		printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
-		return;
-	}
+    ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+    size_ip = IP_HL(ip)*4;
+    if (size_ip < 20) {
+        printf("   * Invalid IP header length: %u bytes\n", size_ip);
+        return;
+    }
+    tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
+    size_tcp = TH_OFF(tcp)*4;
+    if (size_tcp < 20) {
+        printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
+        return;
+    }
 
-    printf("%x\n", tcp->th_sport);
     if ((tcp->th_flags & TH_SYN) && (tcp->th_flags & TH_ACK))
-        printf("port is open\n");
+        printf("open\n");
     if ((tcp->th_flags & TH_RST) && (tcp->th_flags & TH_ACK))
-        printf("port is closed\n");
+        printf("closed\n");
 }
 
-int main(int argc, char *argv[]) {
-    int TMP_port[2] = {631, 80}; // TODO filter
-    int TMP_port_len = 2;
-    char TMP_dest_addr[] = "127.0.0.1"; // TODO
-    char TMP_source_addr[] = "127.0.0.1"; // TODO
-
-    pcap_t *handle;			/* Session handle */
-    char *dev;			/* The device to sniff on */
-    char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
-    struct bpf_program fp;		/* The compiled filter */
-    bpf_u_int32 mask;		/* Our netmask */
-    bpf_u_int32 net;		/* Our IP */
-    struct pcap_pkthdr header;	/* The header that pcap gives us */
-    const u_char *packet;		/* The actual packet */
-
-    struct sockaddr_in addr;
-    int sock;
-
-    if (42/* TODO if interface is not defined */) {
-        /* Define the device */
-        dev = pcap_lookupdev(errbuf);
-        if (dev == NULL) {
-            fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-            return(2);
+void parge_arguments(int argc, char *argv[], char *arg_dest_addr, int *arg_tcp_ports, int *arg_udp_ports, char *iface, int *tcp_len, int *udp_len) {
+    for (int i = 1; i < argc - 1; i += 2) {
+        if (strcmp(argv[i], "-pt") == 0) {
+            char *pt = strtok (argv[i + 1],",");
+            int j = 0;
+            while (pt != NULL) {
+                int a = atoi(pt);
+                arg_tcp_ports[j] = a;
+                pt = strtok (NULL, ",");
+                j++;
+                (*tcp_len)++;
+            }
+        } else if (strcmp(argv[i], "-pu") == 0) {
+            char *pt = strtok (argv[i + 1],",");
+            int j = 0;
+            while (pt != NULL) {
+                int a = atoi(pt);
+                arg_udp_ports[j] = a;
+                pt = strtok (NULL, ",");
+                j++;
+                (*udp_len)++;
+            }
+        } else if (strcmp(argv[i], "-i") == 0) {
+            iface = strcpy(iface, argv[i + 1]);
+        } else {
+            // TODO error
         }
     }
-    dev = "lo"; //TODO
 
-    /* Find the properties for the device */
+    strcpy(arg_dest_addr, argv[argc-1]);
+    if (strcmp(arg_dest_addr, "localhost") == 0)
+        strcpy(arg_dest_addr, "127.0.0.1");
+}
+
+// Source: https://www.binarytides.com/raw-sockets-c-code-linux/
+void fill_ip_header(struct iphdr *iph, char *data, char *source_ip, struct sockaddr_in sin, char *datagram) {
+    iph->ihl = 5;
+    iph->version = 4;
+    iph->tos = 0;
+    iph->tot_len = sizeof (struct iphdr) + sizeof (struct tcphdr) + strlen(data);
+    iph->id = htonl (54321);    //Id of this packet
+    iph->frag_off = 0;
+    iph->ttl = 255;
+    iph->protocol = IPPROTO_TCP;
+    iph->check = 0;     //Set to 0 before calculating checksum
+    iph->saddr = inet_addr ( source_ip );   //Spoof the source ip address
+    iph->daddr = sin.sin_addr.s_addr;
+    iph->check = csum ((unsigned short *) datagram, iph->tot_len);
+}
+
+// Source: https://www.binarytides.com/raw-sockets-c-code-linux/
+void fill_tcp_header(struct tcphdr *tcph, int tcp_port) {
+    tcph->source = htons (42);
+    tcph->dest = htons (tcp_port);
+    tcph->seq = 0;
+    tcph->ack_seq = 0;
+    tcph->doff = 5; //tcp header size
+    tcph->fin=0;
+    tcph->syn=1;
+    tcph->rst=0;
+    tcph->psh=0;
+    tcph->ack=0;
+    tcph->urg=0;
+    tcph->window = htons (5840);    /* maximum allowed window size */
+    tcph->check = 0;    //leave checksum 0 now, filled later by pseudo header
+    tcph->urg_ptr = 0;
+}
+
+// Source: https://www.binarytides.com/raw-sockets-c-code-linux/
+void fill_pseudo_header(struct pseudo_header *psh, char *source_ip, struct sockaddr_in sin, char *data) {
+    psh->source_address = inet_addr( source_ip );
+    psh->dest_address = sin.sin_addr.s_addr;
+    psh->placeholder = 0;
+    psh->protocol = IPPROTO_TCP;
+    psh->tcp_length = htons(sizeof(struct tcphdr) + strlen(data) );
+}
+
+// Source: https://www.tcpdump.org/pcap.html
+int main(int argc, char *argv[]) {
+    int *arg_tcp_ports = malloc(sizeof(int) * 100);
+    int *arg_udp_ports = malloc(sizeof(int) * 100);
+    int arg_tcp_ports_len = 0;
+    int arg_udp_ports_len = 0;
+    char* arg_dest_addr = malloc(sizeof(char) * 100);
+    char* arg_source_addr = "127.0.0.1";
+    pcap_t *handle;
+    char *dev;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    struct bpf_program fp;
+    bpf_u_int32 mask;
+    bpf_u_int32 net;
+
+    int sock;
+
+    char *iface = malloc(sizeof(char) * 100);
+    parge_arguments(argc, argv, arg_dest_addr, arg_tcp_ports, arg_udp_ports, iface, &arg_tcp_ports_len, &arg_udp_ports_len);
+
+    // PCAP LOOKUPDEV
+    if (strcmp(arg_dest_addr, "127.0.0.1")) {
+        if (!iface) {
+            dev = pcap_lookupdev(errbuf);
+            if (dev == NULL) {
+                fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+                return(2);
+            }
+        } else {
+            dev = iface;
+        }
+    } else {
+        dev = "lo"; //TODO
+    }
+
+    // PCAP LOOKUPNET
     if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
         fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
         net = 0;
         mask = 0;
     }
-    (void) mask;
-    printf("iface:%s\n", dev);
 
-    /* Open the session in promiscuous mode */
+    // PCAP OPEN_LIVE
     handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
     if (handle == NULL) {
         fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
         return(2);
     }
 
+    // SOCKET CREATION
+    sock = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+    if (sock == -1) {
+        printf("Error opening socket\n");
+        return -1;
+    }
 
-    //
-    //
-    //
-    // SCAN
-    //
-    //
-    //
-
-    for (int i = 0; i < TMP_port_len; i++) {
-
-        // SOCKET CREATION
-        sock = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
-        if (sock == -1) {
-            printf("Error opening socket\n");
-            return -1;
-        }
-
-        //Datagram to represent the packet
+    for (int i = 0; i < arg_tcp_ports_len; i++) {
+        //DECLARATIONS
         char datagram[4096] , source_ip[32] , *data , *pseudogram;
-
-        //zero out the packet buffer
         memset (datagram, 0, 4096);
-
-        //IP header
         struct iphdr *iph = (struct iphdr *) datagram;
-
-        //TCP header
         struct tcphdr *tcph = (struct tcphdr *) (datagram + sizeof (struct ip));
         struct sockaddr_in sin;
         struct pseudo_header psh;
 
-        //Data part
+        //DATA - fill random data
         data = datagram + sizeof(struct iphdr) + sizeof(struct tcphdr);
-        strcpy(data , "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        strcpy(data , "Did you every hear the tragedy of Darth Plagueis the Wise?");
+        strcpy(source_ip , arg_source_addr);
 
-        //some address resolution
-        strcpy(source_ip , "127.0.0.1");
+        //SIN
         sin.sin_family = AF_INET;
-        sin.sin_port = htons(TMP_port[i]);
-        sin.sin_addr.s_addr = inet_addr ("127.0.0.1");
+        sin.sin_port = htons(arg_tcp_ports[i]);
+        sin.sin_addr.s_addr = inet_addr (arg_dest_addr);
 
-        //Fill in the IP Header
-        iph->ihl = 5;
-        iph->version = 4;
-        iph->tos = 0;
-        iph->tot_len = sizeof (struct iphdr) + sizeof (struct tcphdr) + strlen(data);
-        iph->id = htonl (54321);    //Id of this packet
-        iph->frag_off = 0;
-        iph->ttl = 255;
-        iph->protocol = IPPROTO_TCP;
-        iph->check = 0;     //Set to 0 before calculating checksum
-        iph->saddr = inet_addr ( source_ip );   //Spoof the source ip address
-        iph->daddr = sin.sin_addr.s_addr;
+        //IP HEADER
+        fill_ip_header(iph, data, source_ip, sin, datagram);
 
-        //Ip checksum
-        iph->check = csum ((unsigned short *) datagram, iph->tot_len);
+        //TCP HEADER
+        fill_tcp_header(tcph, arg_tcp_ports[i]);
 
-        //TCP Header
-        tcph->source = htons (42);
-        tcph->dest = htons (TMP_port[i]);
-        tcph->seq = 0;
-        tcph->ack_seq = 0;
-        tcph->doff = 5; //tcp header size
-        tcph->fin=0;
-        tcph->syn=1;
-        tcph->rst=0;
-        tcph->psh=0;
-        tcph->ack=0;
-        tcph->urg=0;
-        tcph->window = htons (5840);    /* maximum allowed window size */
-        tcph->check = 0;    //leave checksum 0 now, filled later by pseudo header
-        tcph->urg_ptr = 0;
-
-        //Now the TCP checksum
-        psh.source_address = inet_addr( source_ip );
-        psh.dest_address = sin.sin_addr.s_addr;
-        psh.placeholder = 0;
-        psh.protocol = IPPROTO_TCP;
-        psh.tcp_length = htons(sizeof(struct tcphdr) + strlen(data) );
+        //PSEUDO HEADER
+        fill_pseudo_header(&psh, source_ip, sin, data);
 
         int psize = sizeof(struct pseudo_header) + sizeof(struct tcphdr) + strlen(data);
         pseudogram = malloc(psize);
-
         memcpy(pseudogram , (char*) &psh , sizeof (struct pseudo_header));
         memcpy(pseudogram + sizeof(struct pseudo_header) , tcph , sizeof(struct tcphdr) + strlen(data));
-
         tcph->check = csum( (unsigned short*) pseudogram , psize);
-
-        //IP_HDRINCL to tell the kernel that headers are included in the packet
         int one = 1;
         const int *val = &one;
 
-        /* Compile and apply the filter */
-        char* filter_exp = malloc(sizeof(char) * 40);	/* The filter expression */
-        sprintf(filter_exp, "tcp src port %d and tcp dst port 42", TMP_port[i]);
-        printf("filter:%s\n", filter_exp);
+        //FILTER EXPRESSION FOR PCAP
+        char* filter_exp = malloc(sizeof(char) * 40);
+        sprintf(filter_exp, "tcp src port %d and tcp dst port 42", arg_tcp_ports[i]);
+
+        //PCAP COMPILE
         if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
             fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
             return(2);
         }
+
+        //PCAP FILTER
         if (pcap_setfilter(handle, &fp) == -1) {
             fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
             return(2);
         }
 
+        //SET SOCKET
         if (setsockopt (sock, IPPROTO_IP, IP_HDRINCL, val, sizeof (one)) < 0) {
             perror("Error setting IP_HDRINCL");
             exit(0);
         }
 
-        //Send the packet
+        //SEND PACKET
         if (sendto (sock, datagram, iph->tot_len , 0, (struct sockaddr *) &sin, sizeof (sin)) < 0) {
             perror("sendto failed");
-        } else {
-            printf ("Packet Send. Length : %d \n" , iph->tot_len);
         }
 
-        // packet = pcap_next(handle, &header);
-        pcap_loop(handle, 1, myHandler, NULL);
-        // Grab packet
+        printf("%d/dcp: ",arg_tcp_ports[i]);
+        //PCAP LOOP
+        // pcap_loop(handle, 1, my_handler, NULL);
+        bool try_filtered = false;
+        struct pcap_pkthdr header;
+        const u_char *packet;
+        
+        while(42) {
+            packet = pcap_next(handle, &header);
 
-        /* Print its length */
-		printf("Jacked a packet with length of [%d]\n", header.len);
-		/* And close the session */
+            if (packet != NULL) {
+                const struct sniff_ip *ip; /* The IP header */
+                const struct sniff_tcp *tcp; /* The TCP header */
+
+                u_int size_ip;
+                u_int size_tcp;
+
+                ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+                size_ip = IP_HL(ip)*4;
+                if (size_ip < 20) {
+                    printf("   * Invalid IP header length: %u bytes\n", size_ip);
+                    exit(1);
+                }
+                tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
+                size_tcp = TH_OFF(tcp)*4;
+                if (size_tcp < 20) {
+                    printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
+                    exit(1);
+                }
+
+                if ((tcp->th_flags & TH_SYN) && (tcp->th_flags & TH_ACK))
+                    printf("open\n");
+                if ((tcp->th_flags & TH_RST) && (tcp->th_flags & TH_ACK))
+                    printf("closed\n");
+            } else if (try_filtered) {
+                printf("filtered\n");
+                break;
+            } else {
+                try_filtered = true;
+            }
+        break;
+        }
     }
     pcap_close(handle);
-    //
-    //
-    //
-    // SCAN
-    //
-    //
-    //
 
     return(0);
 }
